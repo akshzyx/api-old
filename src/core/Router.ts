e8cff2ada00529689b39a90cf1335b8f58fc92e6
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-01 02:40:13 +0100

    (router) added prometheus middleware

--- src/core/Router.ts
+++ src/core/Router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import promMid from "express-prometheus-middleware";
 import authRouter from "../routers/auth";
 import chartsRouter from "../routers/charts";
 import importRouter from "../routers/import";
@@ -7,6 +8,14 @@ import statusRouter from "../routers/status";
 
 export default async (): Promise<void> => {
   const app = express();
+
+  app.use(
+    promMid({
+      metricsPath: "/metrics",
+      collectDefaultMetrics: true,
+      requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    })
+  );
 
   app.use(authRouter);
 
