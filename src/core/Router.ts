c9fe8131d665b3ad76e83032895fc93c8334388f
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-02 03:26:20 +0100

    (router) removed prometheus middleware

--- src/core/Router.ts
+++ src/core/Router.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import promMid from "express-prometheus-middleware";
 import authRouter from "../routers/auth";
 import chartsRouter from "../routers/charts";
 import importRouter from "../routers/import";
@@ -8,14 +7,6 @@ import statusRouter from "../routers/status";
 
 export default async (): Promise<void> => {
   const app = express();
-
-  app.use(
-    promMid({
-      metricsPath: "/metrics",
-      collectDefaultMetrics: true,
-      requestDurationBuckets: [0.1, 0.5, 1, 1.5],
-    })
-  );
 
   app.use(authRouter);
 
