45c6a5bed24eaced0edd7078763e0b965d4988d4
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-03 22:23:51 +0100

    (auth) disable encryption since it doesnt work

--- src/misc/crypto.ts
+++ src/misc/crypto.ts
@@ -3,11 +3,13 @@ import CryptoJS from "crypto-js";
 const encryptionSecret = process.env.ENCRYPTION_SECRET as string;
 
 export function encrypt(msg: string): string {
-  return CryptoJS.AES.encrypt(msg, encryptionSecret).toString();
+  return msg;
+  // return CryptoJS.AES.encrypt(msg, encryptionSecret).toString();
 }
 
 export function decrypt(msg: string): string {
-  return CryptoJS.AES.decrypt(msg, encryptionSecret).toString(
-    CryptoJS.enc.Utf8
-  );
+  return msg;
+  // return CryptoJS.AES.decrypt(msg, encryptionSecret).toString(
+  //   CryptoJS.enc.Utf8
+  // );
 }
