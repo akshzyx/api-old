7dff1cd1f6c1bf7b73e910697601cd8d4956e26c
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-07 04:00:39 +0100

    (router) fix deprecated redirect

--- src/routers/redirect.ts
+++ src/routers/redirect.ts
@@ -4,15 +4,15 @@ const redirectRouter = Router();
 const apiPrefix = process.env.API_PREFIX;
 
 redirectRouter.get(`${apiPrefix}/redirect/discord`, (req, res) =>
-  res.redirect("https://discord.gg/aV9EtB3", 301)
+  res.redirect(301, "https://discord.gg/aV9EtB3")
 );
 
 redirectRouter.get(`${apiPrefix}/redirect/instagram`, (req, res) =>
-  res.redirect("https://instagram.com/spotistats", 301)
+  res.redirect(301, "https://instagram.com/spotistats")
 );
 
 redirectRouter.get(`${apiPrefix}/redirect/twitter`, (req, res) =>
-  res.redirect("https://twitter.com/spotistats", 301)
+  res.redirect(301, "https://twitter.com/spotistats")
 );
 
 export default redirectRouter;
