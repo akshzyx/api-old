5dc3395bfc3021a71956120983b8adfedb35ae33
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-07 03:40:41 +0100

    (import) import website now uses the new import api

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,109 +1,87 @@
 class ImportStepper {
   constructor() {
     this.SERVER_URL = `${location.origin}/api/v1`;
-    this._token = localStorage.getItem("token");
+    this._code = localStorage.getItem("code");
     this.user;
     this.currentStep;
     this.loader = new Loader();
     this.modalController = new ModalController();
 
-    this.getUser();
+    this.getUser(this._code);
   }
 
-  async getUser() {
+  async getUser(code) {
     this.loader.show();
-    const res = await fetch(`${this.SERVER_URL}/import/userinfo`, {
-      headers: { Authorization: this._token },
-    }).then((res) => res.json());
+    let res;
+    if (code) {
+      res = await fetch(`${this.SERVER_URL}/import/code`, {
+        method: "post",
+        body: new URLSearchParams(`code=${code.toUpperCase()}`),
+      }).then((res) => res.json());
 
-    if (res.success) {
-      this.user = res.data;
-      $(".import-code").text(this.user.importCode);
-      // this.listImports();
-    } else {
-      delete this._token;
-      delete this.user;
+      if (res.success) {
+        this._code = code;
+        localStorage.setItem("code", this._code);
+        this.user = res.data;
+        $("#import-code input[type='text'")
+          .val(this._code)
+          .prop("readonly", true);
+      } else {
+        delete this._code;
+        delete this.user;
+      }
     }
 
-    this.currentStep = this._token == undefined ? 0 : 1;
+    this.currentStep = this._code == undefined ? 0 : 1;
     this.updateInterface();
     this.loader.hide();
-  }
 
-  async listImports() {
-    const res = await fetch(`${this.SERVER_URL}/import/${this.user.id}/list`, {
-      headers: { Authorization: this.user.importCode },
-    }).then((res) => res.json());
-
-    if (res.success === true && res.data?.imports?.length > 0) {
-      res.data.imports.forEach((e) => {
-        console.log(e);
-        const created = new Date(e.timeCreated);
-        $("#prev-imported").show();
-        $("#prev-imported").append(
-          `<b>${created.toLocaleDateString()}</b><br>`
-        );
-        $("#prev-imported").append(`- ${e.name}`);
-      });
-    }
+    return res;
   }
 
   async updateInterface() {
-    // $(".step-current").removeClass("step-current");
     $($(".step-hidden")[this.currentStep]).addClass("step-current");
 
-    if (this._token && this.user) {
-      $("#auth-check button").addClass("red");
-      $("#auth-check button").text("Log out");
+    if (this._code && this.user) {
+      $("#import-code button").addClass("red");
+      $("#import-code button").text("Log out");
       $("#authcode").text(`Logged in as ${this.user.displayName}`);
-      $("#auth-check button").click(() => this.logout());
+      $("#import-code button").click(() => this.logout());
     }
 
     const hasFiles = document.getElementById("files").files.length > 0;
     $("#upload").prop("disabled", !hasFiles);
   }
 
-  async nextStep() {
+  async nextStep(e) {
+    if (e && e.preventDefault) e.preventDefault();
     this.loader.show();
     if (this.currentStep == 0) {
       await this.step1();
     } else if (this.currentStep == 1) {
       await this.step2();
-      this.currentStep++;
     }
     this.updateInterface();
     this.loader.hide();
   }
 
   async step1() {
-    if (!this._token || !this.user) {
-      return new Promise((resolve, _) => {
-        const loginWindow = window.open(
-          `${this.SERVER_URL}/auth/redirect`,
-          "_blank",
-          "toolbar=yes,scrollbars=yes,resizable=yes,width=600,height=800"
-        );
-
-        loginWindow.onload = async () => {
-          const url = loginWindow.location.href;
-          const token = /#complete\?token=(?<token>ey.*)/.exec(url).groups
-            .token;
-          if (token !== undefined) {
-            this._token = token;
-            localStorage.setItem("token", this._token);
-            await this.getUser();
-            loginWindow.close();
-            resolve();
-          }
-        };
-      });
+    const code = $("#import-code input[type='text']").val();
+    if (code && code.length == 6) {
+      const res = await this.getUser(code);
+      if (res.success) {
+      } else if (res.message == "no user found") {
+        this.modalController.openModal("Invalid code", "Please try try again");
+      } else {
+        this.modalController.openModal("Error", res.message);
+      }
     }
   }
 
   async step2() {
     return new Promise(async (resolve, _) => {
       var form_data = new FormData();
-      form_data.append("token", this._token);
+      form_data.append("code", this._code);
 
       var totalfiles = document.getElementById("files").files.length;
       for (var index = 0; index < totalfiles; index++) {
@@ -121,10 +99,12 @@ class ImportStepper {
       if (res.success) {
         $("#streams-imported").text(res.message);
         $(".import-code").text(res.importCode);
+        $("#upload-files button").prop("disabled", true);
         this.modalController.openModal(
           "Success",
           `${res.message} Please follow the instructions of the next step to download the data to your device.`
         );
+        this.currentStep++;
       } else {
         this.modalController.openModal("Error", res.message);
       }
@@ -133,7 +113,7 @@ class ImportStepper {
   }
 
   async logout() {
-    localStorage.removeItem("token");
+    localStorage.removeItem("code");
     location.reload();
   }
 }
@@ -177,7 +157,6 @@ class ModalController {
 const importStepper = new ImportStepper();
 
 $(document).ready(function () {
-  $(".next-step").click(() => importStepper.nextStep());
   $("#files").change(() => importStepper.updateInterface());
 
   if (location.hash == "#guide") {
