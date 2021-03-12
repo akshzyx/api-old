e95f710ef640c7a93c0ad066bb4d340ab7407d6f
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-12 02:56:51 +0100

    added all new modules to app module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,13 +1,28 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from '../../guards/auth.guard';
+import { AuthModule } from '../auth/auth.module';
+import { ChartsModule } from '../charts/charts.module';
 import { LyricsModule } from '../lyrics/lyrics.module';
+import { PlusModule } from '../plus/plus.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { StatusModule } from '../status/service.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [LyricsModule, PrismaModule],
+  imports: [
+    // CacheModule.register({
+    //   store: redisStore,
+    // }),
+    PrismaModule,
+    AuthModule,
+    LyricsModule,
+    ChartsModule,
+    PlusModule,
+    StatusModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
