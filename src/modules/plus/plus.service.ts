d94f079401457c64243705a233d4a6ff5a1cecf5
Sting Alleman <stingalleman@icloud.com>
2021-03-13 00:42:37 +0100

    feat(plus/service): rewrite inAppPurchase service from old api

--- src/modules/plus/plus.service.ts
+++ src/modules/plus/plus.service.ts
@@ -1,8 +1,64 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
+import iap from 'in-app-purchase';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PlusService {
+  static _iap;
+
+  constructor(private prisma: PrismaService) {
+    const serviceAccount = JSON.parse(
+      readFileSync(
+        join(__dirname, process.env.GOOGLE_SERVICE_ACCOUNT_PATH),
+      ).toString(),
+    );
+    iap.config({
+      appleExcludeOldTransactions: false,
+      applePassword: process.env.APPLE_SHARED_SECRET,
+      googleServiceAccount: {
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key,
+      },
+    });
+  }
+
+  async setup() {
+    await iap.setup();
+  }
+
+  async validate(receipt, user): Promise<boolean> {
+    const validatedData = await iap.validate(receipt);
+    const purchaseData = await iap.getPurchaseData(validatedData);
+
+    const data: iap.PurchasedItem = purchaseData[0];
+
+    let date = Date.now();
+    // @ts-ignore
+    if (typeof data?.purchaseTimeMillis == 'number')
+      // @ts-ignore
+      date = data.purchaseTimeMillis;
+    // @ts-ignore
+    if (typeof data?.purchaseDateMs == 'number') date = data.purchaseDateMs;
+
+    try {
+      await this.prisma.inAppPurchase.create({
+        data: {
+          id: data.transactionId,
+          purchaseDate: new Date(date),
+          googleOrderId: data?.orderId,
+          productId: data.productId,
+          userId: user.id,
+        },
+      });
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
+
   async postReceipt(user: User) {
     return user;
   }
