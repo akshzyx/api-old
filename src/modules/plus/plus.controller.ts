4ac8a44b887ebe9c552eaa058c02c687613ad280
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-13 04:51:33 +0100

    feat(plus): added plus module

--- src/modules/plus/plus.controller.ts
+++ src/modules/plus/plus.controller.ts
@@ -1,19 +1,21 @@
-import { Controller, Post, Param, UseGuards } from '@nestjs/common';
-import { PlusService } from './plus.service';
-import { Response } from '../../interfaces/response';
-import { UserAuthGuard } from 'src/guards/auth.guard';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthInclude } from 'src/decorators/AuthInclude.decorator';
 import { User } from 'src/decorators/user.decorator';
+import { UserAuthGuard } from 'src/guards/auth.guard';
+import { Response } from '../../interfaces/response';
+import { PlusService } from './plus.service';
 
 @Controller('/plus')
-@UseGuards(UserAuthGuard)
 export class PlusController {
   constructor(private plusService: PlusService) {}
 
+  @UseGuards(UserAuthGuard)
+  @AuthInclude({ inAppPurchase: true })
   @Post()
-  async postReceipt(@User() user): Promise<Response> {
+  async postReceipt(@User() user, @Req() req): Promise<Response> {
     return {
       success: true,
-      data: await this.plusService.postReceipt(user),
+      data: await this.plusService.postReceipt(user, req.body),
     };
   }
 }
--- src/modules/plus/plus.service.ts
+++ src/modules/plus/plus.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { readFileSync } from 'fs';
-import iap from 'in-app-purchase';
+import * as iap from 'in-app-purchase';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class PlusService {
-  static _iap;
-
   constructor(private prisma: PrismaService) {
     const serviceAccount = JSON.parse(
       readFileSync(process.env.GOOGLE_SERVICE_ACCOUNT_PATH).toString(),
@@ -26,7 +23,22 @@ export class PlusService {
     await iap.setup();
   }
 
-  async validate(receipt, user): Promise<boolean> {
+  async postReceipt(user, body) {
+    const receipt = body?.receipt;
+
+    const inAppPurchase = await this.validate(user, receipt);
+    if (inAppPurchase || (user.inAppPurchase?.length > 0 && !user.isPlus)) {
+      user = await this.prisma.user.update({
+        where: { id: user.id },
+        data: { isPlus: true },
+        include: { inAppPurchase: true },
+      });
+    }
+
+    return user;
+  }
+
+  async validate(user, receipt): Promise<boolean> {
     const validatedData = await iap.validate(receipt);
     const purchaseData = await iap.getPurchaseData(validatedData);
 
@@ -54,9 +66,5 @@ export class PlusService {
       return false;
     }
     return true;
-  }
-
-  async postReceipt(user: User) {
-    return user;
   }
 }
