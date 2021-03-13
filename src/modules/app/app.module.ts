fac7f1e96fc2e123d8ce3fd59c90c6a994d590c8
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-13 04:18:36 +0100

    feat(import): added import module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
 import { ChartsModule } from '../charts/charts.module';
+import { CloudStorageModule } from '../cloudStorage/cloudStorage.module';
+import { ImportModule } from '../import/import.module';
 import { LyricsModule } from '../lyrics/lyrics.module';
 import { PlusModule } from '../plus/plus.module';
 import { PrismaModule } from '../prisma/prisma.module';
@@ -14,7 +16,9 @@ import { AppService } from './app.service';
   imports: [
     RedisModule,
     PrismaModule,
+    CloudStorageModule,
     AuthModule,
+    ImportModule,
     LyricsModule,
     ChartsModule,
     PlusModule,
--- src/modules/import/import.controller.ts
+++ src/modules/import/import.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Req,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { AuthInclude } from 'src/decorators/AuthInclude.decorator';
+import { User } from 'src/decorators/user.decorator';
+import { UserAuthGuard } from 'src/guards/auth.guard';
+import { Response } from '../../interfaces/response';
+import { ImportService } from './import.service';
 
-@Controller()
-export class ImportController {}
+@Controller('/import')
+export class ImportController {
+  constructor(private importService: ImportService) {}
+
+  @UseGuards(UserAuthGuard)
+  @AuthInclude({ settings: true, apiClient: true })
+  @Get('/code')
+  async getCode(@User() user): Promise<Response> {
+    return {
+      success: true,
+      data: await this.importService.getCode(user),
+    };
+  }
+
+  @Post('/code')
+  async postCode(@Req() req): Promise<Response> {
+    return {
+      success: true,
+      data: await this.importService.postCode(req.body),
+    };
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Get('/download')
+  async getDownloadURL(@User() user, @Req() req): Promise<Response> {
+    return {
+      success: true,
+      data: await this.importService.getDownloadURL(user, req.query),
+    };
+  }
+
+  @Post('/upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  async upload(@UploadedFiles() files, @Req() req): Promise<Response> {
+    return {
+      success: true,
+      data: await this.importService.upload(files, req.body),
+    };
+  }
+}
