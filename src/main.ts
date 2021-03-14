5611ef4bab89d62f95bdc8f93d4653bb13e427de
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-14 23:48:34 +0100

    fix(core): enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './modules/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.setGlobalPrefix(process.env.API_PREFIX || '');
   await app.listen(process.env.API_PORT || 3000);
 }
