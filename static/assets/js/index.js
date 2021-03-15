a89488352b556a4fbf4c44720ac6bbdce7d910f2
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-15 18:16:32 +0100

    feat(import): enable lint on static import scripts

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,7 +1,7 @@
 class ImportStepper {
   constructor() {
     this.SERVER_URL = `${location.origin}/api/v1`;
-    this._code = localStorage.getItem("code");
+    this._code = localStorage.getItem('code');
     this.user;
     this.currentStep;
     this.loader = new Loader();
@@ -15,18 +15,18 @@ class ImportStepper {
     let res;
     if (code) {
       res = await fetch(`${this.SERVER_URL}/import/code`, {
-        method: "post",
+        method: 'post',
         body: new URLSearchParams(`code=${code.toUpperCase()}`),
       }).then((res) => res.json());
 
       if (res.success && res.data) {
         this._code = code;
-        localStorage.setItem("code", this._code);
+        localStorage.setItem('code', this._code);
         this.user = res.data;
-        $("#display-name").text(`Logged in as ${this.user.displayName}`)
+        $('#display-name').text(`Logged in as ${this.user.displayName}`);
         $("#import-code input[type='text']")
           .val(this._code)
-          .prop("disabled", true);
+          .prop('disabled', true);
       } else {
         delete this._code;
         delete this.user;
@@ -41,17 +41,17 @@ class ImportStepper {
   }
 
   async updateInterface() {
-    $($(".step-hidden")[this.currentStep]).addClass("step-current");
+    $($('.step-hidden')[this.currentStep]).addClass('step-current');
 
     if (this._code && this.user) {
-      $("#import-code button").addClass("red");
-      $("#import-code button").text("Log out");
-      $("#authcode").text(`Logged in as ${this.user.displayName}`);
-      $("#import-code button").click(() => this.logout());
+      $('#import-code button').addClass('red');
+      $('#import-code button').text('Log out');
+      $('#authcode').text(`Logged in as ${this.user.displayName}`);
+      $('#import-code button').click(() => this.logout());
     }
 
-    const hasFiles = document.getElementById("files").files.length > 0;
-    $("#upload").prop("disabled", !hasFiles);
+    const hasFiles = document.getElementById('files').files.length > 0;
+    $('#upload').prop('disabled', !hasFiles);
   }
 
   async nextStep(e) {
@@ -71,10 +71,10 @@ class ImportStepper {
     if (code && code.length == 6) {
       const res = await this.getUser(code);
       if (res.success) {
-      } else if (res.message == "no user found") {
-        this.modalController.openModal("Invalid code", "Please try try again");
+      } else if (res.message == 'no user found') {
+        this.modalController.openModal('Invalid code', 'Please try try again');
       } else {
-        this.modalController.openModal("Error", res.message);
+        this.modalController.openModal('Error', res.message);
       }
     }
   }
@@ -82,74 +82,74 @@ class ImportStepper {
   async step2() {
     return new Promise(async (resolve, _) => {
       var form_data = new FormData();
-      form_data.append("code", this._code.toUpperCase());
+      form_data.append('code', this._code.toUpperCase());
 
-      var totalfiles = document.getElementById("files").files.length;
+      var totalfiles = document.getElementById('files').files.length;
       for (var index = 0; index < totalfiles; index++) {
         form_data.append(
-          "files",
-          document.getElementById("files").files[index]
+          'files',
+          document.getElementById('files').files[index],
         );
       }
 
       const res = await fetch(`${this.SERVER_URL}/import/upload`, {
-        method: "post",
+        method: 'post',
         body: form_data,
       }).then((res) => res.json());
 
       if (res.success) {
-        $("#streams-imported").text(res.data.message);
-        $("#upload-files button").prop("disabled", true);
+        $('#streams-imported').text(res.data.message);
+        $('#upload-files button').prop('disabled', true);
         this.modalController.openModal(
-          "Success",
-          `${res.data.message} Please follow the instructions of the next step to download the data to your device.`
+          'Success',
+          `${res.data.message} Please follow the instructions of the next step to download the data to your device.`,
         );
         this.currentStep++;
       } else {
-        this.modalController.openModal("Error", res.message);
+        this.modalController.openModal('Error', res.message);
       }
       resolve();
     });
   }
 
   async logout() {
-    localStorage.removeItem("code");
+    localStorage.removeItem('code');
     location.reload();
   }
 }
 
 class Loader {
   show() {
-    $("#overlay").show();
+    $('#overlay').show();
   }
 
   hide() {
-    $("#overlay").hide();
+    $('#overlay').hide();
   }
 }
 
 class ModalController {
   constructor() {
-    this.template = $("#modal-template").html();
+    this.template = $('#modal-template').html();
   }
 
   openModal(title, body) {
     var modal = $(this.template);
 
-    modal.find(".title").text(title);
-    modal.find(".body").text(body);
+    modal.find('.title').text(title);
+    modal.find('.body').text(body);
 
-    modal.find("span.close").on("click", function () {
+    modal.find('span.close').on('click', function () {
       $(this).parent().parent().remove();
     });
 
-    modal.on("click", function (e) {
+    modal.on('click', function (e) {
       if (e.target == this) {
         $(this).remove();
       }
     });
 
-    $("body").append(modal);
+    $('body').append(modal);
     modal.show();
   }
 }
@@ -157,10 +157,10 @@ class ModalController {
 const importStepper = new ImportStepper();
 
 $(document).ready(function () {
-  $("#files").change(() => importStepper.updateInterface());
+  $('#files').change(() => importStepper.updateInterface());
 
-  if (location.hash == "#guide") {
-    $("#page-title").text("Import Guide");
-    $(".guide-hide").hide();
+  if (location.hash == '#guide') {
+    $('#page-title').text('Import Guide');
+    $('.guide-hide').hide();
   }
 });
