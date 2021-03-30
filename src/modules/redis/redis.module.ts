e571444ef4f2ba0ec06cad8d76ee8f8a497122b6
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-30 04:13:35 +0200

    feat(redis): add password to redis

--- src/modules/redis/redis.module.ts
+++ src/modules/redis/redis.module.ts
@@ -9,6 +9,7 @@ import * as redisStore from 'cache-manager-redis-store';
       store: redisStore,
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT || 6379,
+      auth_pass: process.env.REDIS_PASSWORD,
     }),
   ],
   providers: [RedisService],
