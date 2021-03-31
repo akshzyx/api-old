470c3b36e3475484c66b036780fddc8dc92e4442
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-03-31 04:04:11 +0200

    feat(core): load dotenv in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 import { NestFactory } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { AppModule } from './modules/app/app.module';
