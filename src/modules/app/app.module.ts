b259622c4278b9539a4b4de9accce0b5ab75a54f
Sjoerd Bolten <sjoerdabolten@gmail.com>
2021-04-02 21:42:00 +0200

    feat(core): add friends and spotify module to app

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
 import { ChartsModule } from '../charts/charts.module';
 import { CloudStorageModule } from '../cloudStorage/cloudStorage.module';
+import { FriendsModule } from '../friends/friends.module';
 import { ImportModule } from '../import/import.module';
 import { LyricsModule } from '../lyrics/lyrics.module';
 import { MiscModule } from '../misc/misc.module';
 import { PlusModule } from '../plus/plus.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RedisModule } from '../redis/redis.module';
+import { SpotifyModule } from '../spotify/spotify.module';
 import { StatusModule } from '../status/service.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -25,6 +27,8 @@ import { AppService } from './app.service';
     PlusModule,
     StatusModule,
     MiscModule,
+    FriendsModule,
+    SpotifyModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
